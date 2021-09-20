c435d436b3d87854992f3f8bb866a0c4467ae14d
NARIMANTASPC\Samsung <narimantas97@gmail.com>
2021-09-20 16:14:29 +0300

    Namų darbas yra inputpage ir input test folderiuose

---
+++ Page/CheckBoxDemoInputPage.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatinisTestavimas.Page
+{
+    class CheckBoxDemoInputPage
+    {
+        private static IWebDriver _Driver;
+
+        private IWebElement _clickOnThisCheckbox => _Driver.FindElement(By.Id("isAgeSelected"));
+
+        private IWebElement _checkBoxIsChecked => _Driver.FindElement(By.Id("txtAge"));
+
+        private IWebElement _checkBox1 => _Driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(3) > label > input"));
+
+        private IWebElement _checkBox2 => _Driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(4) > label > input"));
+
+        private IWebElement _checkBox3 => _Driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(5) > label > input"));
+
+        private IWebElement _checkBox4 => _Driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(6) > label > input"));
+
+        private IWebElement _uncheckAll => _Driver.FindElement(By.Id("#check1"));
+
+        public CheckBoxDemoInputPage(IWebDriver webDriver)
+        {
+            _Driver = webDriver;
+        }
+        public void ClickOnSingleCheckboxDemoCheckbox()
+        {
+            _clickOnThisCheckbox.Click();
+            
+        }
+
+        public void CheckSingleCheckboxDemoMessage(string TextPage)
+        {
+            Assert.AreEqual(_checkBoxIsChecked.Text, TextPage, "Text is different");
+        }
+
+        public void ClickCheckbox1()
+        {
+            _checkBox1.Click();
+        }
+        public void ClickCheckbox2()
+        {
+            _checkBox2.Click();
+        }
+        public void ClickCheckbox3()
+        {
+            _checkBox3.Click();
+        }
+
+        public void ClickCheckbox4()
+        {
+            _checkBox4.Click();
+        }
+
+        public void CheckIfbuttonSayUncheckAll(string result)
+        {
+            Assert.AreEqual(_uncheckAll.Text, result, "Result is diffreant");
+        }
+
+        public void ClickUncheckAll()
+        {
+            _uncheckAll.Click();
+        }
+    }
+}
---
+++ Test/CheckBoxDemoTest.cs
@@ -0,0 +1,50 @@
+﻿using AutomatinisTestavimas.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatinisTestavimas.Test
+{
+    class CheckBoxDemoTest
+    {
+        private static IWebDriver _Driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _Driver = new ChromeDriver();
+            _Driver.Manage().Window.Maximize();
+            _Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _Driver.Url = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html";
+
+            WebDriverWait wait = new WebDriverWait(_Driver, TimeSpan.FromSeconds(10));
+
+            IWebElement popup = _Driver.FindElement(By.Id("at-cv-lightbox-close"));
+            wait.Until(e => e.FindElement(By.Id("at-cv-lightbox-close")).Displayed);
+            popup.Click();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _Driver.Close();
+        }
+
+        [Test]
+        public static void CheckBoxDemo()
+        {
+            CheckBoxDemoInputPage page = new CheckBoxDemoInputPage(_Driver);
+            string text = "Success - Check box is checked";
+            string result = "Uncheck All";
+
+            page.CheckSingleCheckboxDemoMessage(text);
+            page.CheckIfbuttonSayUncheckAll(result);
+        }
+    }
+}
